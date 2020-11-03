63330dcf6d95c99a242c0ce4f0b4d17c787458cc
Tyler Whitehouse <twhitehouse@hawkmail.hccfl.edu>
2020-11-03 14:38:59 -0500

    added classes/license/readme updated gitignore

---
+++ src/main/java/teamviewer/AddEmployees.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamviewer;
+
+/**
+ *gets:
+ * name from add employee window
+ * start/end time from add employee window
+ * 
+ * @author tyler
+ */
+public class AddEmployees {
+    
+}
