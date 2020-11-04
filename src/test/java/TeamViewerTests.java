b5ad4981900c419b095979e6803b0dd80d2b3966
Tyler Whitehouse <twhitehouse@hawkmail.hccfl.edu>
2020-11-04 18:41:45 -0500

    added some stubs

--- src/test/java/TeamViewerTests.java
+++ src/test/java/TeamViewerTests.java
@@ -1,17 +1,19 @@
+import static org.junit.jupiter.api.*;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package teamviewer;
 
 /**
- *gets:
- * name from add employee window
- * start/end time from add employee window
- * 
+ *
  * @author tyler
  */
-public class AddEmployees {
+public class TeamViewerTests 
+{
+    
+    @Test
+    
     
 }
